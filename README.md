81406df623882e78eac01dd17c4aee3b61f706bb
Sapphire0912 <iris228520@gmail.com>
2024-11-06 10:29:19 +0800

    finish template html

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-## 專案名稱： Financial-management
+## 專案名稱： Financial-management 個人理財專案
 
-個人理財專案
+## 補充
+
+- Reference 資料夾內容是參考資料，並採用 Flask + request + sqlite3 製作
+
+## 使用技術
+
+- **Flask**
+- **Python**
+- **sqlite3**
---
+++ reference/index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/cash')
+def cash_form():
+    return render_template('cash.html')
+
+
+@app.route('/stock')
+def stock_form():
+    return render_template('stock.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
