3da077e881c65014311c838abbc6f0ec574b2abc
Sapphire0912 <iris228520@gmail.com>
2024-11-06 15:40:05 +0800

    finish stock function

--- reference/index.py
+++ reference/index.py
@@ -25,8 +25,8 @@ def home():
     # 取得資料庫資料
     conn = get_db()
     cursor = conn.cursor()
-    result = cursor.execute("""SELECT * FROM cash""")
-    cash_result = result.fetchall()
+    cash_result = cursor.execute("""SELECT * FROM cash""")
+    cash_result = cash_result.fetchall()
 
     # 計算台幣與美金的總額
     tw_dollars = 0
@@ -43,13 +43,70 @@ def home():
     # 計算總共現金
     total_dollars = math.floor(tw_dollars + us_dollars * USD_to_TWD_rate)
 
+    # 取得所有股票資訊 (此處可以用 SQL 語法去抓取資料)
+    stock_result = cursor.execute("""SELECT * FROM stock""")
+    stock_result = stock_result.fetchall()
+    unique_stock_list = []
+    for data in stock_result:
+        if data[1] not in unique_stock_list:
+            unique_stock_list.append(data[1])
+
+    # 計算股票總市價
+    total_stock_value = 0
+
+    # 計算單一股票資訊
+    stock_info = []
+    for stock in unique_stock_list:
+        result = cursor.execute(
+            """SELECT * FROM stock WHERE stock_id=?""", (stock, ))
+        result = result.fetchall()
+        stock_cost = 0  # 單一股票總花費
+        shares = 0  # 單一股票股數
+        for d in result:
+            shares += d[2]
+            stock_cost += d[2] * d[3] + d[4] + d[5]
+
+        # 取得目前股價資訊
+        url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo=" + stock
+        response = requests.get(url)
+        data = response.json()
+        price_array = data['data']
+        current_price = float(price_array[-1][6].replace(',', ''))
+
+        # 單一股票總市值
+        total_value = int(current_price * shares)
+        total_stock_value += total_value
+
+        # 單一股票的平均成本
+        avg_cost = round(stock_cost / shares, 2)
+
+        # 單一股票報酬率
+        rate_of_return = round((total_value - stock_cost)
+                               * 100 / stock_cost, 2)
+
+        stock_info.append(
+            {
+                'stock_id': stock,
+                'stock_cost': stock_cost,
+                'total_value': total_value,
+                'avg_cost': avg_cost,
+                'shares': shares,
+                'current_price': current_price,
+                'rate_of_return': rate_of_return
+            })
+
+    for stock in stock_info:
+        stock['value_percentage'] = round(
+            stock['total_value'] * 100 / total_stock_value, 2)
+
     # 傳送至前端的物件資訊
     datas = {
         'total': total_dollars,
         'rate': USD_to_TWD_rate,
         'USD': us_dollars,
         'TWD': tw_dollars,
-        'cash_result': cash_result
+        'cash_result': cash_result,
+        'stock_info': stock_info,
     }
 
     return render_template('index.html', data=datas)
@@ -109,7 +166,25 @@ def stock_form():
 
 @app.route('/stock', methods=['POST'])
 def submit_stock():
-    pass
+    # 取得股票資訊、日期等
+    stock_id = request.values["stock-id"]
+    stock_num = request.values["stock-num"]
+    stock_price = request.values["stock-price"]
+    processing_fee = request.values["processing-fee"] if request.values["processing-fee"] != '' else 0
+    tax = request.values["tax"] if request.values["tax"] != '' else 0
+    date = request.values["date"]
+
+    # 更新 db data
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        """INSERT into stock (stock_id, stock_num, stock_price, processing_fee, tax, date_info) values (?, ?, ?, ?, ?, ?)""",
+        (stock_id, stock_num, stock_price, processing_fee, tax, date)
+    )
+    conn.commit()
+
+    # 將使用者導回主頁面
+    return redirect("/")
 
 
 if __name__ == "__main__":
